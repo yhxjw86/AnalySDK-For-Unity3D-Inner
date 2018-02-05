639f4890122d0b055d6818dedbfb5134269a7f60
yhxjw86 <yhxjw86@gmail.com>
2018-02-05 11:32:15 +0800

    实现方法
    实现接口

--- AnalySDKForUnity3D/Assets/Plugins/Android/AnalySDK/AndroidAnalySDKImpl.cs
+++ AnalySDKForUnity3D/Assets/Plugins/Android/AnalySDK/AndroidAnalySDKImpl.cs
@@ -8,53 +8,100 @@ namespace com.analysdk.unity3d
 	#if UNITY_ANDROID
 	public class AndroidAnalySDKImpl : AnalySDKImpl 
 	{
+		private AndroidJavaClass androidJavaClass;
 		
-	public override void trackEvent (string eventName, Hashtable customParams)
-	{
-
-	}
-
-	public override void setLocation (LocationService location)
-	{
-	float latitude = location.lastData.latitude;
-	float longitude = location.lastData.longitude;
-	}
-
-	public override void trackPayEvent (ALSDKPayEvent payEvent)
-	{
-
-	}
-
-	public override void userRegister (ALSDKUser newUser)
-	{
-
-	}
-
-	public override void userLogin (ALSDKUser loginUser)
-	{
-
-	}
-
-	public override void userUpdate (ALSDKUser updateUser)
-	{
-
-	}
-
-	public override void roleCreate (ALSDKRole newRole)
-	{
-
-	}
-
-	public override void roleLogin (ALSDKRole loginRole)
-	{
-
-	}
-
-	public override void roleUpdate (ALSDKRole updateRole)
-	{
-
-	}
+		public override void trackEvent (string eventName, Hashtable customParams)
+		{
 			
+			androidJavaClass = new AndroidJavaClass ("com/mob/game/CoCosAPI");
+		    String json = MiniJSON.jsonEncode(configs);
+			if (androidJavaClass != null) {
+				androidJavaClass.CallStatic ("trackEvent", eventName,json);
+			}
+		}
+
+		public override void setLocation (LocationService location)
+		{
+			float latitude = location.lastData.latitude;
+			float longitude = location.lastData.longitude;
+			androidJavaClass = new AndroidJavaClass ("com/mob/game/CoCosAPI");
+			if (androidJavaClass != null) {
+				androidJavaClass.CallStatic ("setLocation", latitude, longitude);
+			}
+		}
+
+		public override void trackPayEvent (ALSDKPayEvent payEvent)
+		{
+			Hashtable hashtable = payEvent.HashtableValue;
+			androidJavaClass = new AndroidJavaClass ("com/mob/game/CoCosAPI");
+			String json = MiniJSON.jsonEncode(hashtable);
+			if (androidJavaClass != null) {
+				androidJavaClass.CallStatic ("trackPayEvent",json);
+			}
+		}
+
+		public override void userRegister (ALSDKUser newUser)
+		{
+			Hashtable hashtable = newUser.HashtableValue;
+			androidJavaClass = new AndroidJavaClass ("com/mob/game/CoCosAPI");
+			String json = MiniJSON.jsonEncode(hashtable);
+			if (androidJavaClass != null) {
+				androidJavaClass.CallStatic ("userRegister",json);
+			}
+		}
+
+		public override void userLogin (ALSDKUser loginUser)
+		{
+			Hashtable hashtable = loginUser.HashtableValue;
+			androidJavaClass = new AndroidJavaClass ("com/mob/game/CoCosAPI");
+			String json = MiniJSON.jsonEncode(hashtable);
+			if (androidJavaClass != null) {
+				androidJavaClass.CallStatic ("userLogin",json);
+			}
+
+		}
+
+		public override void userUpdate (ALSDKUser updateUser)
+		{
+			Hashtable hashtable = updateUser.HashtableValue;
+			androidJavaClass = new AndroidJavaClass ("com/mob/game/CoCosAPI");
+			String json = MiniJSON.jsonEncode(hashtable);
+			if (androidJavaClass != null) {
+				androidJavaClass.CallStatic ("userUpdate",json);
+			}
+
+		}
+
+		public override void roleCreate (ALSDKRole newRole)
+		{
+			Hashtable hashtable = newRole.HashtableValue;
+			androidJavaClass = new AndroidJavaClass ("com/mob/game/CoCosAPI");
+			String json = MiniJSON.jsonEncode(hashtable);
+			if (androidJavaClass != null) {
+				androidJavaClass.CallStatic ("roleCreate",json);
+			}
+		}
+
+		public override void roleLogin (ALSDKRole loginRole)
+		{
+			Hashtable hashtable = loginRole.HashtableValue;
+			androidJavaClass = new AndroidJavaClass ("com/mob/game/CoCosAPI");
+			String json = MiniJSON.jsonEncode(hashtable);
+			if (androidJavaClass != null) {
+				androidJavaClass.CallStatic ("roleLogin",json);
+			}
+		}
+
+		public override void roleUpdate (ALSDKRole updateRole)
+		{
+			Hashtable hashtable = updateRole.HashtableValue;
+			androidJavaClass = new AndroidJavaClass ("com/mob/game/CoCosAPI");
+			String json = MiniJSON.jsonEncode(hashtable);
+			if (androidJavaClass != null) {
+				androidJavaClass.CallStatic ("roleUpdate",json);
+			}
+
+		}
 
 	}
 	#endif
